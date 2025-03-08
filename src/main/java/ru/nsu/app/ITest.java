fb30eb8a4ddf76149d9408ae64271ef0128b7793
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2025-03-08 12:57:34 +0700

    Combine with java poet

---
+++ src/main/java/ru/nsu/app/ITest.java
@@ -0,0 +1,10 @@
+package ru.nsu.app;
+
+import processor.HierarchyRoot;
+
+@HierarchyRoot
+public interface ITest {
+    void voidFn();
+
+    int getNum();
+}
--- src/main/java/ru/nsu/app/Main.java
+++ src/main/java/ru/nsu/app/Main.java
@@ -2,7 +2,6 @@ package ru.nsu.app;
 
 public class Main {
     public static void main(String[] args) {
-        TestGen t = new TestGen();
-        t.new_test_method();
+        ITestRoot t = new ITestRoot();
     }
 }
